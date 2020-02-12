083a0bb957646c34f2a7591f063a2c95318bca63
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-02-12 20:12:38 -0300

    Implementação do schema de pokemon

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { WELCOME_MESSAGE } from './../constants/poke-api.constants';
 
+import { Pokemon } from './../models/pokemon.model';
+
 export class PokeService {
 
     public welcomeMessage(req: Request, res: Response) {
